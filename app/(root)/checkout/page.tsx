0dcb6856a9298613efb361bbf785531dd9994ec8
TuanKiet091202 <tuankieta4hbt@gmail.com>
2024-10-18 19:07:54 +0700

    fix url

--- app/(root)/checkout/page.tsx
+++ app/(root)/checkout/page.tsx
@@ -57,7 +57,7 @@ const AddressForm = () => {
          console.log('Cart Items:', JSON.parse(cartItems || '[]'));
          console.log('Shipping Address:', JSON.parse(shippingAddress || '{}'));
 
-         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
+         const res = await fetch(`https://comet-admin-tau.vercel.app/api/checkout`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             credentials: 'include',
