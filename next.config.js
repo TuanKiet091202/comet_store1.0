0724b54306a97ea91c1e4938dca3cbecf9b02a22
TuanKiet091202 <tuankieta4hbt@gmail.com>
2024-10-19 18:26:10 +0700

    fix url

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-// next.config.js
 module.exports = {
-   async rewrites() {
+   async headers() {
       return [
          {
-            source: '/api/:path*',
-            destination: 'https://comet-admin-tau.vercel.app/api/:path*',
+            // Khớp tất cả các routes API
+            source: "/api/:path*",
+            headers: [
+               { key: "Access-Control-Allow-Credentials", value: "true" },
+               { key: "Access-Control-Allow-Origin", value: "https://comet-store.vercel.app" },
+               { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+               { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Type" },
+            ],
          },
       ];
    },
